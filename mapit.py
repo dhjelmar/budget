172baee1dd08251f0a5278ce990c8f466875d1f2
dhjelmar <dlhjel@gmail.com>
2023-05-20 16:04:34 -0400

    when no match in map, changed from fatal error to set it to be Xbudget

--- mapit.py
+++ mapit.py
@@ -4,16 +4,34 @@ def mapit(dataframe, map):
     '''
 
     import pandas as pd
+    import regex as re
+
     df = dataframe.copy()
     df = pd.merge(df, map, how='left', on='AccountNum')
 
     ## flag any line items from budget without Category assigned
     nan_values = df[df['Category'].isna()]
     if len(nan_values) != 0:
-        print('')
-        print('FATAL ERROR: Following budget entries are missing a Category assignment in map.xlsx file')
-        print(nan_values)
-        sys.exit()
+    #    print('')
+    #    print('FATAL ERROR: Following budget entries are missing a Category assignment in map.xlsx file')
+    #    print(nan_values)
+    #    sys.exit()
+
+        ## classify anything that does not have Category defined in map
+        mask = df['Category'].isna()
+        df.loc[mask, 'Category'] = 'Xbudget'
+        df.loc[mask, 'GreenSheet'] = 'Xbudget'
+        df.loc[mask, 'Committee'] = 'Xbudget'
+        df.loc[mask, 'SourceOfFunds'] = 'Xbudget'
+        
+        ## set InOrOut based on dollar fields being positive or negative
+        dollarfields = [x for x in df.columns if re.findall(r'Amount',x)]
+        df['dollarsum'] = 0   # initialize new variable
+        for i in dollarfields:
+            df.loc[mask, 'dollarsum'] = df.loc[mask, 'dollarsum'] + df.loc[mask, i]
+        df.loc[(df.InOrOut.isna() & df.dollarsum >= 0), 'InOrOut'] = 'In' 
+        df.loc[(df.InOrOut.isna() & df.dollarsum <  0), 'InOrOut'] = 'Out' 
+
 
     print(df)
 
