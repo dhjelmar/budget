52ebfddf3a379b9d7e45d5a7edc37f4a5069dbab
dhjelmar <dlhjel@gmail.com>
2023-05-28 17:40:33 -0400

    rewritten to work on actualb and actualc instead of table so adjustments would be factored into plots

--- linearadj.py
+++ linearadj.py
@@ -1,37 +1,55 @@
-def linearadj(filename, table, startb, startc, endb):
+def linearadj(filename, dfin, start, end):
     '''
     Read filename to...
     '''
-    df = table.copy()
     import pandas as pd
+    import numpy as np
+    from date_eom import date_eom
+    from dateeom import dateeom
+
+    # %%
+    df = dfin.copy()
+
+    ## monthly cumsum for df and dfc then flatten
+    df = dateeom(df)
+    df = df.pivot_table(index=['AccountNum', 'Account', 'Date'], values=['Amount'], aggfunc=np.sum) # cumsum
+    df = df.reset_index()    # flatten
+
+    # %%
     ## read budget_linear.xlsx
     linear = pd.read_excel(filename)
     linear.AccountNum = linear.AccountNum.astype(str)
+    linear_list = list(linear.AccountNum.unique())
 
-    ## apply map
-    ## linear, missing = mapit(linear, map)
+    ## determine correct columns from linear for budgetb and budgetc and redefine linear dataframe
+    budget = linear['budget'+str(start.year)]
+    linear = pd.DataFrame({'AccountNum' : linear.AccountNum, 
+                           'budget'     : budget})
 
-    ## pull corresponding lines from df
-    ## dfvals = df.loc[df['AccountNum'].isin(linear.AccountNum)].copy()
+    # %%
+    ## expand linear to have an entry for each AccountNum for each month
+    dicts = []
+    for AccountNum in linear_list:
+        for month in range(1,13):
+            if month > end.month:
+                break           # exit for loop
+            eom = date_eom(start.year, month)
+            ## values[0] is needed in the following to remove the value from a series
+            amount = linear.loc[linear.AccountNum == AccountNum, 'budget'].values[0] * month/12
+            dicts.append(
+                {
+                    'Date': eom,
+                    'Account': AccountNum + ' linear adjustment',
+                    'Amount': amount,
+                    'AccountNum': AccountNum
+                }
+            )
+    linear2 = pd.DataFrame(dicts)
 
-    ## pull corresponding lines from df
-    linear = pd.merge(linear, df, how='left', on='AccountNum')
-
-    ## determine YTD for linear adjustments
-    budgetb = linear['budget'+str(startb.year)]
-    budgetc = linear['budget'+str(startc.year)]
-    linear['Current Month'] = round(budgetb              / 12 - linear['Current Month'], 2)
-    linear['Last YTD']      = round(budgetc              / 12 - linear['Last YTD'], 2)
-    linear.YTD              = round(budgetb * endb.month / 12 - linear.YTD, 2)
-    linear.Budget           = 0
-    linear.Account = linear['AccountNum'].astype(str) + " linear adjustment"
-
-    ## only keep same columns from linear as had in df so can combine
-    linear = linear[df.columns]
-
-    ## add to df
-    ## rbind = pd.concat([df1, df2], axis=0)
-    df = pd.concat([df, linear], axis=0)
-    df.index = range(len(df))
+    # %%
+    ## combine, sort, and renumber
+    df = pd.concat([df, linear2], axis=0)  # rbind
+    df = df.sort_values(by = ['AccountNum', 'Date'], ascending=True, na_position='last')  # sort
+    df.index = range(len(df))              # renumber dataframe
 
     return df
