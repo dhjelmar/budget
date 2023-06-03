48fbc1165a85c43994fb7e3d42f04a43139566b0
dhjelmar <dlhjel@gmail.com>
2023-06-02 20:25:55 -0400

    added plt.close('all') to close open figures; added plotcsv() function

--- plotit.py
+++ plotit.py
@@ -28,9 +28,12 @@ def plotit(x, y, data, hue=None, hue_order=None, legendloc='best',
     if filename != None: { plt.savefig(filename) } # this write the figure to file filename
     plt.figure()                                  # this plots and closes the figure
 
+    plt.close('all')
+
 
 def plotit_test():
     import pandas as pd
+    from plotit import plotit
     df = pd.DataFrame({'col1': [1,2,3,4],
                        'col2': [10,15,10,12],
                        'label': ['one', 'two', 'one', 'two']})
@@ -44,3 +47,44 @@ def plotit_test():
     #             hue='label', style='label', markers=[",","o"])
 
 # plotit_test()
+
+def plotcsv(InOrOut, Category, csv=None, df=None, figsize=(6,4)):
+    import pandas as pd
+    from read_map import read_map
+    from mapit import mapit
+    import seaborn as sns
+    from matplotlib import pyplot as plt
+
+    ## read dataframe and map
+    if csv != None:
+        df = pd.read_csv(csv)
+        df['Date'] = pd.to_datetime(df['Date']).dt.date
+        df['AccountNum'] = df['AccountNum'].astype(str)
+    
+    map, map_duplicates = read_map()
+
+    ## add "InOrOut" and "Category" to df
+    df, missing = mapit(df, map)   
+
+    ## select requested InOrOut and Category
+    df = df.loc[(df.InOrOut == InOrOut) & (df.Category == Category)]
+
+    ## calculate cumsum
+    df = df.sort_values('Date')
+    df['CumSum'] = df.Amount.cumsum()
+
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.lineplot(ax=ax, data=df, x='Date', y='Amount', markers=',',
+                 errorbar=None)\
+       .set(title=Category)
+    sns.lineplot(ax=ax, data=df, x='Date', y='CumSum', markers='o',
+                 errorbar=None)
+    plt.close('all')
+
+    df = df[['Date', 'AccountNum', 'Amount', 'CumSum']]
+
+    return df
+
+## plotcsv('In', 'Contributions', 'actualb.csv')
+## plotcsv('In', 'Contributions', 'actualc.csv')
+
