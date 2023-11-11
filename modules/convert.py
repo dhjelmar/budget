af541358bee1b5b6f69447c0c60a20c490c02c9d
dhjelmar <dlhjel@gmail.com>
2023-11-11 15:31:19 -0500

    added read and merge with prior year

--- modules/convert.py
+++ modules/convert.py
@@ -1,20 +1,16 @@
 #%%
-def convert(file='input_files/budget_2024_office.xlsx', 
-            columns=['Account', 'Budget_2024', 'Source_of_Funds', 'Recurring', 'Budget_2023', 'Current_Balance', 'Difference', 'Comments'],
-            dollar_columns=[2,5]):
-    ## READ OFFICE VERSION OF BUDGET DATA INTO DATAFRAME: budget
+def convert(file='../input_files/budget_2024_office.xlsx',
+            col_rename=['Account', 'Budget_2024', 'Source_of_Funds', 'Recurring', 'Budget_2023', 'Current_Balance', 'Difference', 'Comments'],
+            budget_columns=[1,4],
+            col_out   =['InOrOut', 'AccountNum', 'Account', 'Budget_2024']):
+    ## READ OFFICE VERSION OF BUDGET DATA INTO DATAFRAME: df
 
     import pandas as pd
     import numpy as np
 
     ## read budget file and fix column names
     df = pd.read_excel(file)
-    if (columns == None):
-        ## use column names from Excel but replace special characters with "_"
-        df.columns = df.columns.str.replace('[ ,!,@,#,$,%,^,&,*,(,),-,+,=,\',\"]', '_', regex=True)
-        columns = df.columns
-    else:
-        df.columns = columns
+    df.columns = col_rename
 
     ## remove any row where Source_of_Funds is NaN
     df = df.dropna(subset = ['Source_of_Funds'])
@@ -26,18 +22,31 @@ def convert(file='input_files/budget_2024_office.xlsx',
 
     ## create column indicating whether account is income (In) or expense (Out)
     df['InOrOut'] = np.where(df['AccountNum'] < 5000, 'In', 'Out')
-    for i in dollar_columns:
+    df['AccountNum'] = df['AccountNum'].apply(str) # convert back to string for merge later
+    for i in budget_columns:
         df.iloc[:,i] = np.where(df['InOrOut'] == 'In', df.iloc[:,i], -df.iloc[:,i])
 
     ## move InOrOut and AccountNum to front of dataframe
-    newcols = ['InOrOut'] + ['AccountNum'] + columns
-    df = df[newcols]
+    df = df[col_out]
 
     return df
 
 #%%
-budget = convert(file='input_files/budget_2024_office.xlsx', 
-                 columns=['Account', 'Budget_2024', 'Source_of_Funds', 'Recurring', 'Budget_2023', 'Current_Balance', 'Difference', 'Comments'],
-                 dollar_columns=[2,5])
-budget[['InOrOut', 'AccountNum', 'Account', 'Budget_2023', 'Budget_2024']]
-# %%
+budget = convert()
+
+#%%
+## read prior year
+import pandas as pd
+df = pd.read_excel('../input_files/budget_2023.xlsx')
+df = df[['Account', 'Budget']]
+
+#%%
+df['AccountNum'] = df.Account.str.extract('(^\d+a|^\d+)')
+df.columns = ['Account_2023', 'Budget_2023', 'AccountNum']
+
+
+#%%
+## merge dataframes (must be on sring)
+dfnew = pd.merge(df, budget, how='outer', on='AccountNum')
+dfnew = [['InOrOut', 'AccountNum', 'Account', 'Budget_2024', 'Account_2023', 'Budget_2023']]
+
