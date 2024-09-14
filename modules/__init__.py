1102c5ea619df7c65c715c14ca19b3de6797d4e7
dlhjel <dlhjel@gmail.com>
2024-09-14 11:20:48 -0400

    changed to use __init__.py to import module functions in budget.py

---
+++ modules/__init__.py
@@ -0,0 +1,21 @@
+from modules.set_dates import set_dates
+from modules.read_map import read_map
+from modules.read_budget import read_budget
+from modules.linearadj import linearadj
+from modules.mapit import mapit
+from modules.icon import icon     # gives access to all function icon() in icon.py; e.g., icon(startb, endb, startc, endc)
+from modules.linearadj import linearadj
+import modules.dollars as dollars            # gives access to all fucntions in dollars.py; e.g., dollars.to_num('-$4')
+from modules.percent import percent
+from modules.highlight import highlight
+from modules.write_excel import write_excel
+from modules.dupacct import dupacct
+from modules.dateeom import dateeom
+from modules.tabletotals import tabletotals
+from modules.plotit import plotit
+from modules.dfplot_inout import dfplot_inout
+from modules.rmdir import rmdir
+from modules.category_plot import category_plot
+from modules.category_table import category_table
+from modules.tableit import tableit
+from modules.inconsistent import inconsistent
